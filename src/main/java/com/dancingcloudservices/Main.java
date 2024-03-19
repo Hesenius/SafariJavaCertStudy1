39d3ccc6023258d9add27cea9b6e4d2145c8a7ac
simon <simon@dancingcloudservices.com>
2024-03-19 12:10:27 -0600

    Initial

--- src/main/java/com/dancingcloudservices/Main.java
+++ src/main/java/com/dancingcloudservices/Main.java
@@ -4,9 +4,6 @@ sealed interface X permits Main {}
 public record Main(String msg) implements X {
     public static void main(String[] args) {
         X x = new Main("Hello Java 17 world!");
-        System.out.println(switch (x) {
-            case Main(String msg) -> msg;
-            default -> "Uh oh!";
-        });
+        System.out.println(x instanceof Main m ? m.msg : "Uh oh!");
     }
 }
